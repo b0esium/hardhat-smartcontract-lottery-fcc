d7a21ef9f671bd7e5f9064e7857f61008ced3eeb
b0esium <b0esium@protonmail.com>
2023-07-12 17:50:16 +0200

    Add tests

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -8,10 +8,10 @@ module.exports = async function ({ getNamedAccounts, deployment }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
-    let vrfCoordinatorV2Address, subscriptionId
+    let vrfCoordinatorV2Address, subscriptionId, VRFCoordinatorV2Mock
 
     if (developmentChains.includes(network.name)) {
-        const VRFCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        VRFCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
         vrfCoordinatorV2Address = VRFCoordinatorV2Mock.address
         const transactionResponse = await VRFCoordinatorV2Mock.createSubscription()
         const transactionReceipt = await transactionResponse.wait(1)
@@ -41,6 +41,11 @@ module.exports = async function ({ getNamedAccounts, deployment }) {
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     })
+
+    if (developmentChains.includes(network.name)) {
+        await VRFCoordinatorV2Mock.addConsumer(subscriptionId, raffle.address)
+        log("Consumer is added")
+    }
 
     if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
         log("Verifying...")
